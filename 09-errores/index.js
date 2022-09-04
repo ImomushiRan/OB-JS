05ff83ad02711dda8b6edd900c5d5b4f7b023c59
ImomushiRan <george.villarroel@outlook.es>
2022-09-04 01:31:52 -0300

    ej9

---
+++ 09-errores/index.js
@@ -0,0 +1,7 @@
+const logger = require('./logger');
+
+/* logger.log("Hola estoy saliendo en pantalla"); */
+logger.info("Hola esto es un mensaje informativo")
+logger.warn("Hola esto es un mensaje de advertencia")
+logger.debug("Hola esto es un mensaje debug")
+logger.error("Hola esto es un error")
