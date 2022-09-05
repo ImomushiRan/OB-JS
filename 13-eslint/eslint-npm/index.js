ad55faacbaa2af7d64dffd61d0deb0bad490dc16
ImomushiRan <george.villarroel@outlook.es>
2022-09-05 12:38:45 -0300

    ej13

---
+++ 13-eslint/eslint-npm/index.js
@@ -0,0 +1,18 @@
+// Este es el archivo qu evamos a utilizar
+const nombre = "Gorka";
+
+/* eslint-disable */
+const persona2 = 'Maria'
+
+/* eslint-enable */
+
+
+// En esta linea quiero tener comillas simples (Quiero me desactives la regla de las comillas dobles)
+const nuevoString = 'Esto es un nuevo string'; //eslint-disable-line
+
+/* eslint-disable-next-line indent */
+const string2 = "Mas strings";
+
+const persona3 = "Maria";
+
+const nombre3 = "Julian";
